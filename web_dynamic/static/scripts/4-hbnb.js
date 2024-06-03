1e6c5d59737e2aa44a70f750b986c63cced915ed
brio kchr <b.kchr4@gmail.com>
2024-06-03 08:33:49 -0700

    Completed task 4 and 5 on AJX Post and API filtering

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  function getSelectedAmenities () {
+    const amenitiesText = $('.amenities > h4').text();
+    const selectedAmenities = amenitiesText ? amenitiesText.split(', ') : [];
+    return selectedAmenities;
+  }
+  $('button:contains("Search")').click(function () {
+    const selectedAmenities = getSelectedAmenities();
+
+    $.ajax({
+      type: 'POST',
+      ContentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ amenities: selectedAmenities }),
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      success: function (data) {
+        data.forEach(function (place) {
+          const article = $('<article>');
+
+          const titleBox = $('<div>').addClass('title_box');
+          const placeName = $('<h2>').text(place.name);
+          const priceByNight = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
+          titleBox.append(placeName, priceByNight);
+
+          const information = $('<div>').addClass('information');
+          const maxGuest = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
+          const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Room${place.number_rooms !== 1 ? 's' : ''}`);
+          const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
+          information.append(maxGuest, numberRooms, numberBathrooms);
+
+          const description = $('<div>').addClass('description').html(place.description);
+          article.append(titleBox, information, description);
+          $('section.places').append(article);
+        });
+      },
+      error: function () {
+        console.error('Error fetching places');
+      }
+    });
+  });
+});
